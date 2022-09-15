9c4d35a25f7b5da995b5b639e4283dafb45dc483
alli <abc@xyz.com>
2022-09-15 02:57:33 +0000

    fixed class name

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,4 @@
-punlic class HelloWorld{
+public class HelloWorld{
   public static void main(String args[]){
     System.out.println("Hello Welcome to DevOps training!");
     System.out.println("This is my second commit from github");
